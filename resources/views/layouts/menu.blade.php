ae2aff6fe41701e3648603b5ad64db6952ed0477
zeyniep <75210764+zeyniep@users.noreply.github.com>
2022-01-08 20:03:30 +0300

    dsfs

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -10,7 +10,7 @@
     <div >
     <ul >
       <li><a href="/ogrencianasayfa">Ana sayfa</a></li>
-      <li><a href="/basvurularım">Başvurularım</a></li>
+      <li><a href="/basvurularim">Başvurularım</a></li>
       <li><a href="/basvuruyap">Başvuru Yap</a></li>
       <li><a href="/kisiselbilgiler">Kişisel Bilgiler</a></li>
       <li><a href="/">Çıkış</a></li>
